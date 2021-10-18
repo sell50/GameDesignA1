b24492845b97ec84701f367e86ef314509a3e89c
Chantal Santos <chantalsantos7@gmail.com>
2021-10-18 16:45:01 -0400

    Added texture to capsule character

--- A2 - Group 10/Assets/Scripts/MovePlayer.cs
+++ A2 - Group 10/Assets/Scripts/MovePlayer.cs
@@ -15,8 +15,9 @@ public class MovePlayer : MonoBehaviour
     private float turnSmoothTime = 0.1f;
 
     private float turnSmoothVelocity;
-    private float jumpHeight = 5.0f;
+    private float jumpHeight = 2.0f;
     private bool jump;
+    private int jumpNo;
 
     private void Start()
     {
@@ -25,22 +26,39 @@ public class MovePlayer : MonoBehaviour
 
     private void Update()
     {
+        if (controller.isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0;
+        }
+        
         var horizontal = Input.GetAxisRaw("Horizontal");
         var vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+        var direction = new Vector3(horizontal, 0, vertical).normalized;
         if (direction.magnitude >= 0.1)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            var moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * playerSpeed * Time.deltaTime);
         }
         
         if (Input.GetButtonDown("Jump"))
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+            /*//isGrounded doesn't work
+            if (controller.isGrounded)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+                jumpNo = 0;
+            }
+
+            if (!controller.isGrounded && jumpNo <= 2)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+                jumpNo++;
+            }*/
         }
 
         playerVelocity.y += gravity * Time.deltaTime;
