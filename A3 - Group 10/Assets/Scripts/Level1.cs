350b4b955203259450cbe6f189f55c1c53922b42
Kat <pacek@uwindsor.ca>
2021-11-03 18:37:16 -0400

    Edit Priest, Level 1

--- A3 - Group 10/Assets/Scripts/Level1.cs
+++ A3 - Group 10/Assets/Scripts/Level1.cs
@@ -6,9 +6,8 @@ using UnityEngine.UI;
 
 namespace Characters
 {
-    public class Level2 : BasicCharacter
+    public class Level1 : BasicCharacter
     {
-
         public Text healthBoss, healthMage, healthWarrior, healthMD, healthRog, health Prist;
         Boss boss = new Boss();
         MoonkinDruid md = new MoonkinDruid();
@@ -16,15 +15,15 @@ namespace Characters
         Warrior war = new Warrior();
         Rogue rog = new Rogue();
         Priest pri = new Priest();
-        //PlayerPrefs.SetInt("highscore", 0);
+        PlayerPrefs.SetInt("highscore", 0);
 
         void Update()
         {
-            healthBoss.text = "Boss: " + boss.health + "\nDamage: ";
+            healthBoss.text = "Boss: " + boss.health + "\nDamage: " + boss.GetTotalDamage();
             healthMage.text = "Mage: " + mage.health + "\nDamage: " + mage.GetTotalDamage();
-            healthMD.text = "Moonkin Druid: " + md.health + "\nDamage: ";
-            healthWarrior.text = "Warrior: " + war.health + "\nDamage: ";
-            healthRog.text = "Rogue: " + rog.health + "\nDamage: ";
+            healthMD.text = "Moonkin Druid: " + md.health + "\nDamage: " + md.GetTotalDamage();
+            healthWarrior.text = "Warrior: " + war.health + "\nDamage: " + war.GetTotalDamage();
+            healthRog.text = "Rogue: " + rog.health + "\nDamage: " + rog.GetTotalDamage();
             healthPri.text = "Priest: " + pri.health;
 
             boss.DealDamage(mage);
@@ -41,17 +40,17 @@ namespace Characters
 
             if (boss.health == 0 || mage.health == 0 || md.health == 0 || war.health == 0)
             {
-                boss.GameObject.SetActive(false);
-                mage.GameObject.SetActive(false);
+                boss.gameObject.SetActive(false);
+                mage.gameObject.SetActive(false);
                 md.GameObject.SetActive(false);
-                rog.GameObject.SetActive(false);
-                war.GameObject.SetActive(false);
-                pri.GameObject.SetActive(false);
+                rog.gameObject.SetActive(false);
+                war.gameObject.SetActive(false);
+                pri.gameObject.SetActive(false);
             }
 
             if (boss.GetTotalDamage() > highscore)
             {
-                //PlayerPrefs.SetInt("highscore", boss.GetTotalDamage());
+                PlayerPrefs.SetInt("highscore", boss.GetTotalDamage());
             }
         }
     }
