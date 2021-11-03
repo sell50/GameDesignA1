a28e21c983cc6bcf4fb7ef053a83b5dc6e34f904
sameeksha-nair <sameeksha.canada@gmail.com>
2021-11-03 19:54:39 -0400

    Update Level2.cs

--- A3 - Group 10/Assets/Scripts/Level2.cs
+++ A3 - Group 10/Assets/Scripts/Level2.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using Characters;
 
-
-namespace Characters
-{
-    public class Level2 : BasicCharacter
+    public class Level2 : MonoBehaviour
     {
 
         public Text healthBoss, healthMage, healthWarrior, healthMD, healthPriest, healthRogue;
@@ -16,15 +15,16 @@ namespace Characters
         Warrior war = new Warrior();
         Priest priest = new Priest();
         Rogue rogue = new Rogue();
+        int timestep = 0;
 
         void Update()
         {
-            healthBoss.text = "Boss: " + boss.health + "\nDamage: " + boss.GetTotalDamage(); ;
-            healthMage.text = "Mage: " + mage.health + "\nDamage: " + mage.GetTotalDamage();
-            healthMD.text = "Moonkin Druid: " + md.health + "\nDamage: " + md.GetTotalDamage(); ;
-            healthWarrior.text = "Warrior: " + war.health + "\nDamage: " + war.GetTotalDamage(); ;
-            healthPriest.text = "Priest: " + priest.health + "\nDamage: " + priest.GetTotalDamage(); ;
-            healthRogue.text = "Rogue: " + rogue.health + "\nDamage: " + rogue.GetTotalDamage(); ;
+            healthBoss.text = "Boss: " + boss.health + "\nDamage: " + boss.GetTotalDamageDone(); ;
+            healthMage.text = "Mage: " + mage.health + "\nDamage: " + mage.GetTotalDamageDone();
+            healthMD.text = "Moonkin Druid: " + md.health + "\nDamage: " + md.GetTotalDamageDone(); ;
+            healthWarrior.text = "Warrior: " + war.health + "\nDamage: " + war.GetTotalDamageDone(); ;
+            healthPriest.text = "Priest: " + priest.health + "\nDamage: " + priest.GetTotalDamageDone(); ;
+            healthRogue.text = "Rogue: " + rogue.health + "\nDamage: " + rogue.GetTotalDamageDone(); ;
             boss.DealDamage(mage); boss.DealDamage(md); boss.DealDamage(war); boss.DealDamage(priest); boss.DealDamage(rogue);
             mage.DealDamage(boss);
             md.DealDamage(boss);
@@ -33,7 +33,7 @@ namespace Characters
             rogue.DealDamage(boss);
             if (boss.health == 0 || mage.health == 0 || md.health == 0 || war.health == 0 || priest.health==0 || rogue.health==0)
             {
-
+               // Disable
             }
 
             if (war.health <= 1500)
@@ -41,12 +41,27 @@ namespace Characters
                 int heal = Random.Range(1, 2);
                 switch (heal)
                 {
-                    case 1: { priest.SmallHeal(war); mana += 5; break; }
-                    case 2: { priest.BigHeal(war); mana += 10; break; }
+                    case 1: { priest.SmallHeal(war); priest.mana += 5; break; }
+                    case 2: { priest.BigHeal(war); priest.mana += 10; break; }
                 }
             }
 
+            CSVWriter();
+        }
 
+        void CSVWriter()
+        {
+            string filename = "Level2.csv";
+            TextWriter tw = new StreamWriter(filename, true);
+           
+            tw.WriteLine("Boss" + "," + boss.health);
+            tw.WriteLine("Warrior" + "," + boss.health);
+            tw.WriteLine("Rogue" + "," + boss.health);
+            tw.WriteLine("Mage" + "," + boss.health);
+            tw.WriteLine("Moonkin Druid" + "," + boss.health);
+            tw.WriteLine("Priest" + "," + boss.health);
+            timestep++;
+            tw.Close();
         }
     }
-}
+
