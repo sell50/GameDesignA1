6723fb60c0c664a92a1e9a096462015dbc9befbb
Kat <pacek@uwindsor.ca>
2021-11-03 17:18:03 -0400

    add Priest

---
+++ A3 - Group 10/Assets/Characters/Priest.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+namespace Characters
+{
+    public class Priest : BasicCharacter
+    {
+
+        void Start()
+        {
+            health = 900;
+            mana = 1000;
+        }
+
+
+        void Update()
+        {
+            if (health < 0)
+            {
+
+                var chooseCharacter = Random.Range(1, 6);
+
+                /*if (chooseCharacter == 1 || chooseCharacter == 2)
+                {
+                    SmallHeal(Priest);
+                }
+
+                if (chooseCharacter == 3)
+                {
+                    SmallHeal(Warrior);
+                }
+
+                if (chooseCharacter == 4)
+                {
+                    SmallHeal(Rogue);
+                }
+
+                if (chooseCharacter == 5)
+                {
+                    SmallHeal(Mage);
+                }
+
+                if (chooseCharacter == 6)
+                {
+                    SmallHeal(Moonkin);
+                }
+                */
+                mana += 3;
+
+                
+            }
+
+            else
+            {
+                //end simulation
+            }
+        }
+
+        public void SmallHeal(BasicCharacter opponent)
+        {
+            var heal = 15;
+            opponent.IncreaseHealth(heal);
+            mana -= 5;
+        }
+
+        public void BigHeal(BasicCharacter opponent)
+        {
+            var heal = 25;
+            if (opponent is Warrior)
+            {
+                opponent.IncreaseHealth(heal);
+                mana -= 10;
+            }
+        }
+    }
+}
