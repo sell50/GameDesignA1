740031bef5ed54201f31f21faaa6f612686600a4
Chantal Santos <chantalsantos7@gmail.com>
2021-11-16 00:57:34 -0500

    created Main Menu, worked on level

--- A4 - Group 10/Assets/Scripts/LevelDeath.cs
+++ A4 - Group 10/Assets/Scripts/LevelDeath.cs
@@ -1,18 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Content;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelDeath : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator PlayerDied()
     {
-        
+        yield return new WaitForSeconds(1);
+        //SceneManager.LoadScene();
     }
 }
