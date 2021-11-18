07a49e0dff6f4a6d957569722f4dcb7cb565104e
Chantal Santos <chantalsantos7@gmail.com>
2021-11-17 22:52:08 -0500

    changed player movement to new input system

--- A4 - Group 10/Assets/scripts/MovePlayer.cs
+++ A4 - Group 10/Assets/scripts/MovePlayer.cs
@@ -1,34 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class MovePlayer : MonoBehaviour
+public class MovePlayer : MonoBehaviour, PlayerControls.IMovementActions
 {
+    [Header("Movement Values")]
+    [SerializeField] private float moveSpeed = 5;
+    [SerializeField] private float gravity;
+    [SerializeField] private float jumpheight;
+    [SerializeField] private float horizontalInput;
 
-
-    private float moveSpeed=5;
-    private Vector3 directionZ;
-    private Vector3 directionX;
-    private Vector3 velocity;
+    private Vector3 _direction;
+    private Vector3 _velocity;
+    private float yVelocity;
     private bool isGrounded;
     [SerializeField]private float groundCheckDistance;
     [SerializeField]private LayerMask groundMask;
     private CharacterController controller;
-    [SerializeField] private float gravity;
-    [SerializeField] private float jumpheight;
-    // Start is called before the first frame update
+    
+    
     void Start()
     {
-        controller = GetComponent<CharacterController>();    
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Movement()
+    {
+        isGrounded = controller.isGrounded;
+
+        if (controller.isGrounded)
+        {
+            _velocity = _direction * moveSpeed;
+        }
+        else
+        {
+            _velocity.y -= gravity * Time.deltaTime;
+        }
+        controller.Move(_direction * moveSpeed * Time.deltaTime);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        Move();
+        Movement();
     }
 
-    void Move()
+    /*void Move()
     {
         float movez = Input.GetAxis("Vertical");
         float movex = Input.GetAxis("Horizontal");
@@ -53,5 +71,19 @@ public class MovePlayer : MonoBehaviour
         {
             velocity.y = Mathf.Sqrt(jumpheight * -2 * gravity);
         }
+        velocity.y = Mathf.Sqrt(jumpheight * -2 * gravity);
+                 controller.Move(velocity * Time.deltaTime);
+    }*/
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        Debug.Log(context.ReadValue<Vector2>());
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        //Debug.Log(context.ReadValue<Vector2>().x);
+        var dirX = context.ReadValue<Vector2>().x;
+        var dirZ = context.ReadValue<Vector2>().y;
+        _direction = new Vector3(dirX, 0, dirZ);
     }
 }
