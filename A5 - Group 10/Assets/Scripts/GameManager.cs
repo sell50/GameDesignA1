1f8801878e08ba053771dea017676ff3321a42d1
Kat <pacek@uwindsor.ca>
2021-12-01 21:02:54 -0500

    End Screen Generation

--- A5 - Group 10/Assets/Scripts/GameManager.cs
+++ A5 - Group 10/Assets/Scripts/GameManager.cs
@@ -2,14 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
-{
+{   
+    GameObject endScreen;
+    void Start()
+    {
+        endScreen = GameObject.Find("EndScreen");
+        endScreen.SetActive(false);
+    }
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Debug.Log("This will probably work");
+            endScreen.SetActive(true);
         }
     }
 }
