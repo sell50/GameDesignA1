c9f86115a06132be5beff37c2d1f301636abac58
Kat <pacek@uwindsor.ca>
2021-12-01 20:11:48 -0500

    Add endScreen

---
+++ A5 - Group 10/Assets/Scripts/EndScreenScores.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class EndScreenScores : MonoBehaviour
+{
+    public TMP_Text keysPU, timeEl, lvlComp;
+
+    void start()
+    {
+
+    }
+}
