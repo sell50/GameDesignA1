084cecd96675b8eada48e6ef85e124f390bf0331
Kat <pacek@uwindsor.ca>
2021-12-22 00:23:51 -0500

    Update PowerPush Script

--- Project - Group 10/Assets/Scripts/PowerPush.cs
+++ Project - Group 10/Assets/Scripts/PowerPush.cs
@@ -6,6 +6,7 @@ public class PowerPush : MonoBehaviour
 {
     int abilityCooldown;
     bool isActive;
+    float force = 3;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +32,20 @@ public class PowerPush : MonoBehaviour
         {
             abilityCooldown--;
             yield return new WaitForSeconds(1);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        float force = 3;
+        if (other.gameObject.tag == "enemy")
+        {
+            
+            Vector3 dir = other.contacts[0].point - transform.position;
+            
+            dir = -dir.normalized;
+    
+            GetComponent<Rigidbody>().AddForce(dir*force);
         }
     }
 
